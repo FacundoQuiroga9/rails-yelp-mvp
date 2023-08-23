ba83eb804edcfc6a12f62f0a9c480d08a20f5981
Facundo Quiroga <facu.quirogaga@gmail.com>
2023-08-23 14:16:50 -0500

    Terminado todo lo del controlador restaurants

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  get 'restaurants/index'
+  get 'restaurants/show'
+  get 'restaurants/new'
+  get 'restaurants/create'
+  get 'restaurants/edit'
+  get 'restaurants/update'
+  get 'restaurants/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :restaurants
 end
